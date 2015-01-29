26e18415b58cac33bfac4368c9002aa1750ac6ba
ggthemj <guillaume@oojump.com>
2015-01-29 08:37:07 +0100

    test boulet 2

--- Assets/Scripts/ApplicationModel.cs
+++ Assets/Scripts/ApplicationModel.cs
@@ -5,8 +5,8 @@
 */
 public class ApplicationModel : MonoBehaviour
 {
-	//static string host = "http://54.77.118.214/";					// Adresse du serveur
-	static public string username = "julien";								// Pseudo de l'utilisateur connecté
+	static string host = "http://54.77.118.214/";					// Adresse du serveur
+	static public string username = "";								// Pseudo de l'utilisateur connecté
 	static public string hash = "J8xy9Uz4"; 						// Clé secrète
 	static public Deck selectedDeck;								// Deck à afficher
 }
