6ef046ae33af1e4fcf8cf5ea21cc43d7e868dd57
Yoann Bourges <yoann@oojump.com>
2015-02-06 17:50:33 +0100

    Optimisation du script mycards
    Optimisation pour affichage plus rapide

--- Assets/Scripts/myCards/MyCardsScript.cs
+++ Assets/Scripts/myCards/MyCardsScript.cs
@@ -3,29 +3,34 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
-public class MyCardsScript : MonoBehaviour
+public class Test : MonoBehaviour
 {
 	public GameObject CardObject;												 
 	string[] skillsList;
 	string[] cardTypeList;
-	Card[] cards ;
+	IList<Card> cards;
 	public IList<int> cardsToBeDisplayed ;
 	GameObject[] filtres ;
 	private bool[] togglesCurrentStates;
 	private string valueSkill ;
 	int counterFilters ;
-
-
+	
+	
 	bool dataIsLoaded = false ;
-
+	
 	private IEnumerator Start() 
 	{
 		dataIsLoaded = false;
-		yield return StartCoroutine("getSkillsList");
-		yield return StartCoroutine("getCardTypeList");
-
+		//yield return StartCoroutine("getSkillsList");
+		//yield return StartCoroutine("getCardTypeList");
+		
 		//préparation du tableau de filtres
+		
+		
+		yield return StartCoroutine ("getCards");
+		
 		togglesCurrentStates = new bool[this.cardTypeList.Length - 1];
 		counterFilters = 0;
 		for (int i=0; i<this.cardTypeList.Length-1; i++) {
@@ -33,12 +38,11 @@ public class MyCardsScript : MonoBehaviour
 		}
 		valueSkill = "";
 		
-		yield return StartCoroutine ("getCards");
 		displayCards ();
 		dataIsLoaded = true;
-
+		
 	}
-
+	
 	void OnGUI () {
 		if (dataIsLoaded) {
 			bool toggle;
@@ -63,14 +67,14 @@ public class MyCardsScript : MonoBehaviour
 					}
 				}
 			}
-
+			
 			GUI.Label (new Rect (cadreFiltres.transform.position.x-cadreFiltres.GetComponent<RectTransform>().rect.width/2, cadreFiltres.transform.position.y+ 40 + i * 20-cadreFiltres.GetComponent<RectTransform>().rect.height/2, 150, 20), "FILTRER PAR SKILL");
 			s = GUI.TextField (new Rect (cadreFiltres.transform.position.x-cadreFiltres.GetComponent<RectTransform>().rect.width/2, cadreFiltres.transform.position.y+ 60 + i * 20-cadreFiltres.GetComponent<RectTransform>().rect.height/2, 150, 20), valueSkill);
 			if (s!=valueSkill){
 				if (valueSkill.Length>1){
 					IList<string> matchValues = displaySkills (s);
 					for (int j=0; j<matchValues.Count(); j++) {
-
+						
 						GUI.Label (new Rect (cadreFiltres.transform.position.x-cadreFiltres.GetComponent<RectTransform>().rect.width/2+10, cadreFiltres.transform.position.y+ 80 + i * 20 + j*20-cadreFiltres.GetComponent<RectTransform>().rect.height/2, 150, 20), matchValues[j]);
 					}
 				}
@@ -78,107 +82,66 @@ public class MyCardsScript : MonoBehaviour
 			}
 		}
 	}
-
-	private IEnumerator getSkillsList(){
-		WWWForm form = new WWWForm(); 											// Création de la connexion
-		form.AddField("myform_hash", ApplicationModel.hash); 					// hashcode de sécurité, doit etre identique à celui sur le serveur
-		form.AddField("myform_nick", ApplicationModel.username);
-		
-		WWW w = new WWW("http://54.77.118.214/GarrukServer/get_skills_list.php", form); 				// On envoie le formulaire à l'url sur le serveur 
-		yield return w;
-		if (w.error != null) 
-		{
-			print (w.error); 										// donne l'erreur eventuelle
-		} else {
-			//			print (w.text);
-			this.skillsList = w.text.Split('\n');
-		}
-	}
-
-	private IEnumerator getCardTypeList(){
-		WWWForm form = new WWWForm(); 											// Création de la connexion
-		form.AddField("myform_hash", ApplicationModel.hash); 					// hashcode de sécurité, doit etre identique à celui sur le serveur
-		form.AddField("myform_nick", ApplicationModel.username);
-		
-		WWW w = new WWW("http://54.77.118.214/GarrukServer/get_cards_type_list.php", form); 				// On envoie le formulaire à l'url sur le serveur 
-		yield return w;
-		if (w.error != null) 
-		{
-			print (w.error); 										// donne l'erreur eventuelle
-		} else {
-			//			print (w.text);
-			this.cardTypeList = w.text.Split('\n');
-		}
-	}
-
+	
+	
+	
 	private IEnumerator getCards() {
 		string[] cardsIDS = null;
-		string[] cardInformation = null;
-
+		//string[] cardInformation = null;
+		
 		WWWForm form = new WWWForm(); 											// Création de la connexion
 		form.AddField("myform_hash", ApplicationModel.hash); 					// hashcode de sécurité, doit etre identique à celui sur le serveur
 		form.AddField("myform_nick", ApplicationModel.username);
 		
-		WWW w = new WWW("http://54.77.118.214/GarrukServer/get_cards_by_user.php", form); 				// On envoie le formulaire à l'url sur le serveur 
+		WWW w = new WWW("http://54.77.118.214/GarrukServer/get_mycardspage_data.php", form); 				// On envoie le formulaire à l'url sur le serveur 
 		yield return w;
 		if (w.error != null) 
 		{
 			print (w.error); 										// donne l'erreur eventuelle
 		} else {
-			//print (w.text);
-			cardsIDS = w.text.Split('\n');
+			print (w.text);
+			
+			// cardsIDS = w.text.Split('\n');
+			string[] data=w.text.Split(new string[] { "END" }, System.StringSplitOptions.None);
+			cardsIDS=data[2].Split ('\n');
+			this.skillsList = data[1].Split('\n');
+			this.cardTypeList = data[0].Split('\n');
 		}
 		
-		this.cards = new Card[cardsIDS.Length];
+		this.cards = new List<Card>();
+		int j = 0;
 		this.cardsToBeDisplayed = new List<int>();
-
-		for(int i = 0 ; i < cardsIDS.Length-1 ; i++)         			// On parcourt toutes les cartes de l'utilisateur
+		
+		for(int i = 1 ; i < cardsIDS.Length-1 ; i++)         			// On parcourt toutes les cartes de l'utilisateur
 		{
-			form = new WWWForm(); 											// Création de la connexion
-			form.AddField("myform_hash", ApplicationModel.hash); 					// hashcode de sécurité, doit etre identique à celui sur le serveur
-			form.AddField("myform_nick", ApplicationModel.username);
-			form.AddField ("myform_idcard", cardsIDS[i]);
 			
-			w = new WWW("http://54.77.118.214/GarrukServer/get_cards_informations_by_card.php", form); 				// On envoie le formulaire à l'url sur le serveur 
-			yield return w;
-			if (w.error != null) 
-			{
-				print (w.error); 										// donne l'erreur eventuelle
-			} else {
-//				print (w.text);
-				cardInformation = w.text.Split('\\');
+			string[] cardInformation=cardsIDS[i].Split(new string[] { "\\" }, System.StringSplitOptions.None);
+			if(i!=1){
+				if(!cardsIDS[i].StartsWith(cardsIDS[i-1].Substring(0, 4))){
+					j=j+1;
+					this.cards.Add(new Card(cardInformation[1], System.Convert.ToInt32(cardInformation[2]),System.Convert.ToInt32(cardInformation[7]),System.Convert.ToInt32(cardInformation[9])));	
+					this.cards[j].Skills = new List<Skill>();
+					this.cardsToBeDisplayed.Add(j);
+				}
 			}
-			this.cards[i] = new Card(cardInformation[2], System.Convert.ToInt32(cardInformation[3]), System.Convert.ToInt32(cardInformation[1]),System.Convert.ToInt32(cardInformation[4]));	
-
-			form = new WWWForm(); 											// Création de la connexion
-			form.AddField("myform_hash", ApplicationModel.hash); 					// hashcode de sécurité, doit etre identique à celui sur le serveur
-			form.AddField("myform_nick", ApplicationModel.username);
-			form.AddField ("myform_idcard", cardsIDS[i]);
+			else {
+				this.cards.Add(new Card(cardInformation[1], System.Convert.ToInt32(cardInformation[2]),System.Convert.ToInt32(cardInformation[7]),System.Convert.ToInt32(cardInformation[9])));	
+				this.cards[j].Skills = new List<Skill>();
+			}
+			this.cards[j].Skills.Add(new Skill (cardInformation[10]));
+			print (cardInformation[10]);
 			
-			w = new WWW("http://54.77.118.214/GarrukServer/get_cards_skills_by_card.php", form); 				// On envoie le formulaire à l'url sur le serveur 
-			yield return w;
-			if (w.error != null) 
-			{
-				print (w.error); 										// donne l'erreur eventuelle
-			} else {
-				//print (w.text);
-				cardInformation = w.text.Split('\\');
-			}
-			this.cards[i].Skills = new List<Skill>();
-			foreach(string ci in cardInformation)
-			{
-				this.cards[i].Skills.Add(new Skill(ci));
-			}
-			this.cardsToBeDisplayed.Add(i);
+			
 		}
 	}
-
+	
+	
 	public void displayCards()																// Fonction qui permet d'afficher toutes les cartes à l'initialisation et en fonction des filtres actifs
 	{
 		int x=0;												  						// initialisation des coordonnées des cartes
 		float y=2.5f;
 		int k = 0;
-
+		
 		for(int i = 0 ; i < this.cardsToBeDisplayed.Count ; i++)         			// On parcourt toutes les cartes de l'utilisateur
 		{
 			x= k % 5;																// On calcule les coordonnées de la prochaine carte
@@ -196,34 +159,34 @@ public class MyCardsScript : MonoBehaviour
 			instance.gameObject.name = "Card" + k + "";	
 			k++;
 		}
-
+		
 	}
-
+	
 	public void addCardTypes(int a) {			
 		cleanScreen();
 		if (counterFilters==0){
 			this.clearCards();
 		}
-		for (int i = 0; i < cards.Length - 1; i++) {
+		for (int i = 0; i < cards.Count() - 1; i++) {
 			if (cards[i].IdClass==a){
 				cardsToBeDisplayed.Add(i);
-
+				
 			}
 		}
-
+		
 		displayCards ();							
 	}	
-
+	
 	public void removeCardTypes(int a) {
 		cleanScreen();
 		if (counterFilters == 1) {
 			this.clearCards ();
-			for (int j = 0; j < cards.Length - 1; j++) {
+			for (int j = 0; j < cards.Count() - 1; j++) {
 				cardsToBeDisplayed.Add (j);
 			}
 		}
 		else {
-			for (int j = 0; j < cards.Length - 1; j++) {
+			for (int j = 0; j < cards.Count() - 1; j++) {
 				if (cards [j].IdClass == a) {
 					cardsToBeDisplayed.Remove (j);
 				}
@@ -231,17 +194,17 @@ public class MyCardsScript : MonoBehaviour
 		}
 		displayCards ();												// On lance le calcul après avoir supprimé les cartes de l'écran							
 	}
-
+	
 	public void clearCards() {
 		cardsToBeDisplayed.Clear ();
 	}
-
+	
 	public void cleanScreen(){
 		for (int i = 0; i < cardsToBeDisplayed.Count(); i++) {         			// Il ne peut pas y avoir plus de cartes affichés sur le nombre de cartes possédées par l'utilisateur
 			Destroy(GameObject.Find("Card" + i + ""));	
 		}	
 	}
-
+	
 	public IList<string> displaySkills(string a){
 		IList<string> matchValues = new List<string> ();
 		for (int i = 0; i < skillsList.Length; i++) {  
@@ -252,27 +215,27 @@ public class MyCardsScript : MonoBehaviour
 		}
 		return matchValues;
 	}
-
+	
 	void testType(){
-//		bool cardTypeFilterSucess = true;
-//		bool toFilter;
-//		bool skillsFilterSucess = true;
-//		if (filters.Any ()) {
-//						cardTypeFilterSucess = filters.Any (e => e == cards_information_by_card_data [4]);			// On vérifie si le cardType de la carte est dans la liste des filtres actifs
-//	}
-
-//		if((cardTypeFilterSucess || !filters.Any()) &&								// Tests pour savoir si la carte correspond aux critères compétences et cardType
-//		   (skillsFilterSucess || skillToFilter=="") ){								// Si tous les filtres sont désactivés on peut aussi afficher la carte
-			//toFilter=true;
-//		}
-		}
-
-
+		//		bool cardTypeFilterSucess = true;
+		//		bool toFilter;
+		//		bool skillsFilterSucess = true;
+		//		if (filters.Any ()) {
+		//						cardTypeFilterSucess = filters.Any (e => e == cards_information_by_card_data [4]);			// On vérifie si le cardType de la carte est dans la liste des filtres actifs
+		//	}
+		
+		//		if((cardTypeFilterSucess || !filters.Any()) &&								// Tests pour savoir si la carte correspond aux critères compétences et cardType
+		//		   (skillsFilterSucess || skillToFilter=="") ){								// Si tous les filtres sont désactivés on peut aussi afficher la carte
+		//toFilter=true;
+		//		}
+	}
+	
+	
 	// ----------------  Fonction d'affichage des cartes
-
-
-
-
+	
+	
+	
+	
 	// ----------------  Fonctions pour l'auto complétion
 	
 	
@@ -280,53 +243,53 @@ public class MyCardsScript : MonoBehaviour
 		
 		skill = skill.ToLower ();														// On convertit en minuscule le texte saisi											
 		
-//		AutoCompletion.SetActive(false);												// On masque le panneau de l'auto complétion
-//		if (skill.Length > 0) {
-//			List<string> found = skills.FindAll
-//				( w => w.ToLowerInvariant().StartsWith(skill) );						// On cherche une correspondance dans la liste des compétences possédées par l'utilisateur
-//			if (found.Count > 0 && found[0] != skill){
-//				AutoCompletion.SetActive(true);											// Si une correspondance est trouvée on affiche le panneau
-//				for(int i=0;i<4;i++){													// On met en forme les propositions d'auto complétion
-//					if (i<=found.Count-1){
-//						Proposition[i].SetActive(true);
-//						var texteProposition = Proposition[i].GetComponent<Text>();
-//						texteProposition.text = found[i];
-//					}
-//					else{
-//						Proposition[i].SetActive(false);								// On masque les propositions selon le nombre de correspondances trouvées
-//					}
-//				}
-//				var tailleAutoCompletion = 												// On modifie le panneau d'auto complétion
-//					AutoCompletion.GetComponent<RectTransform>();
-//				tailleAutoCompletion.sizeDelta =										// La taille
-//					new Vector2(160,(found.Count)*25);
-//				tailleAutoCompletion.transform.localPosition =							// et la position
-//					new Vector2(0,-15-(found.Count)*12.5f);
-//			}
-//		}
+		//		AutoCompletion.SetActive(false);												// On masque le panneau de l'auto complétion
+		//		if (skill.Length > 0) {
+		//			List<string> found = skills.FindAll
+		//				( w => w.ToLowerInvariant().StartsWith(skill) );						// On cherche une correspondance dans la liste des compétences possédées par l'utilisateur
+		//			if (found.Count > 0 && found[0] != skill){
+		//				AutoCompletion.SetActive(true);											// Si une correspondance est trouvée on affiche le panneau
+		//				for(int i=0;i<4;i++){													// On met en forme les propositions d'auto complétion
+		//					if (i<=found.Count-1){
+		//						Proposition[i].SetActive(true);
+		//						var texteProposition = Proposition[i].GetComponent<Text>();
+		//						texteProposition.text = found[i];
+		//					}
+		//					else{
+		//						Proposition[i].SetActive(false);								// On masque les propositions selon le nombre de correspondances trouvées
+		//					}
+		//				}
+		//				var tailleAutoCompletion = 												// On modifie le panneau d'auto complétion
+		//					AutoCompletion.GetComponent<RectTransform>();
+		//				tailleAutoCompletion.sizeDelta =										// La taille
+		//					new Vector2(160,(found.Count)*25);
+		//				tailleAutoCompletion.transform.localPosition =							// et la position
+		//					new Vector2(0,-15-(found.Count)*12.5f);
+		//			}
+		//		}
 		
 	}
 	
 	
 	public void autoCompletionClick(string buttonName)									// Fonction qui s'exécute lorsque l'on clique sur une proposition
 	{
-//		StopAllCoroutines ();															// On stoppe les autres fonctions (en fait le déclenchement de cette fonciton, exécute la fonction endTyping (cette instruction sert à la stopper)
-//		cleanScreen();																	// On efface les cartes affichées
-//		filterSkills = GameObject.Find("filterSkills");									// On récupère le texte cliqué par l'utilisateur
-//		var skill = filterSkills.GetComponent<InputField> ();
-//		button = GameObject.Find(buttonName);											// On met à jour le champs de saisie
-//		var texteProposition = button.GetComponent<Text> ();
-//		skill.text = texteProposition.text;
-//		endTyping ();																	// On lance endTyping
+		//		StopAllCoroutines ();															// On stoppe les autres fonctions (en fait le déclenchement de cette fonciton, exécute la fonction endTyping (cette instruction sert à la stopper)
+		//		cleanScreen();																	// On efface les cartes affichées
+		//		filterSkills = GameObject.Find("filterSkills");									// On récupère le texte cliqué par l'utilisateur
+		//		var skill = filterSkills.GetComponent<InputField> ();
+		//		button = GameObject.Find(buttonName);											// On met à jour le champs de saisie
+		//		var texteProposition = button.GetComponent<Text> ();
+		//		skill.text = texteProposition.text;
+		//		endTyping ();																	// On lance endTyping
 	}
 	
 	
 	public void endTyping()																// Fonction qui s'exécute lorsque l'on termine la saisie d'une compétence (ou lorsque l'on clique sur une proposition)
 	{
-//		filterSkills = GameObject.Find("filterSkills");									// On enregistre dans une variable la compétence
-//		var skill = filterSkills.GetComponent<InputField> ();							
-//		skillToFilter = skill.text.ToLower ();											// On convertit en minuscule la variable
-//		cleanScreen();																	// On efface les cartes affichées
-//		//StartCoroutine(calcul ());														// On lance l'affichage des cartes
+		//		filterSkills = GameObject.Find("filterSkills");									// On enregistre dans une variable la compétence
+		//		var skill = filterSkills.GetComponent<InputField> ();							
+		//		skillToFilter = skill.text.ToLower ();											// On convertit en minuscule la variable
+		//		cleanScreen();																	// On efface les cartes affichées
+		//		//StartCoroutine(calcul ());														// On lance l'affichage des cartes
 	}
 }
