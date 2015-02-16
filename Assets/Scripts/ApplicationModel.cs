a3d12db4f7e82f402cf8bbeae45c660edf06b06c
Yoann Bourges <yoann@oojump.com>
2015-02-16 10:46:15 +0100

    Commit boulet

--- Assets/Scripts/ApplicationModel.cs
+++ Assets/Scripts/ApplicationModel.cs
@@ -6,7 +6,7 @@
 public class ApplicationModel : MonoBehaviour
 {
 	static public string host = "http://54.77.118.214/GarrukServer/";			// Adresse du serveur
-	static public string username = "julien";								// Pseudo de l'utilisateur connecté
+	static public string username = "";								// Pseudo de l'utilisateur connecté
 	static public string hash = "J8xy9Uz4"; 						// Clé secrète
 	static public Deck selectedDeck;								// Deck à afficher
 }
