c49d99cd84b8b3f77c193054ced12b3575b5da7f
JulienBx <julien@oojump.com>
2015-03-11 09:33:44 +0100

    Correction MAJ stats hovered card + playing card après buff

--- Assets/Scripts/Game/Skill/Reflexe.cs
+++ Assets/Scripts/Game/Skill/Reflexe.cs
@@ -33,11 +33,11 @@ public class Reflexe : GameSkill
 		Instantiate(gnc.AttackAnim, go.transform.position + new Vector3(0, 0, -2), Quaternion.identity);
 		GameTimeLine.instance.SortCardsBySpeedAfterBuff();
 		GameTimeLine.instance.Arrange();
-		if (GamePlayingCard.instance.gameCard.Equals(gnc))
+		if (GamePlayingCard.instance.gameCard.Card.Equals(gnc.Card))
 		{
 			GamePlayingCard.instance.changeStats();
 		}
-		if (GameHoveredCard.instance.gameCard.Equals(gnc))
+		if (GameHoveredCard.instance.gameCard.Card.Equals(gnc.Card))
 		{
 			GameHoveredCard.instance.changeStats();
 		}
