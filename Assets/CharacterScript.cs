32bdc5bfad934b4cafe77c20af031a5e7c018160
Yoann Bourges <yoann@oojump.com>
2015-03-23 22:49:13 +0100

    Ajout d'un prefab character
    Avec affichage de libellés (responsive design)
    Fonctions clés en main pour gérer les mouvements
    Création d’une scène de test

---
+++ Assets/CharacterScript.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterScript : MonoBehaviour {
+	
+	Vector3 CharacterPositionTopLeft;
+	Vector3 CharacterPositionBottomRight;
+
+	public GUIStyle NamePoliceStyle;
+	public GUIStyle LifePoliceStyle;
+	public GUIStyle SpeedPoliceStyle;
+	public GUIStyle AttackPoliceStyle;
+	public GUIStyle MovePoliceStyle;
+	public GUIStyle QuicknessPoliceStyle;
+	public GUIStyle iconStyle;
+
+	public Texture attackIcon;
+	public Texture moveIcon;
+	public Texture quicknessIcon;
+
+	int widthScreen = Screen.width; 
+	int heightScreen = Screen.height;
+
+	Animator animator;
+
+	string Name;
+	int Life;
+	int Quickness;
+	int Move;
+	int Attack;
+
+	bool toHide=true;
+
+	// Use this for initialization
+	void Start () {
+		animator = transform.GetComponent<Animator> ();
+		setStyles();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Screen.width != widthScreen || Screen.height != heightScreen) {
+			this.setStyles();
+		}
+	}
+
+	void OnGUI ()
+	{
+		if(!toHide){
+			CharacterPositionTopLeft=Camera.main.WorldToScreenPoint(new Vector3(transform.Find("Character_renderer").renderer.bounds.min.x,transform.Find("Character_renderer").renderer.bounds.max.y,0));
+			CharacterPositionBottomRight=Camera.main.WorldToScreenPoint(new Vector3(transform.Find("Character_renderer").renderer.bounds.max.x,transform.Find("Character_renderer").renderer.bounds.min.y,0));
+			GUILayout.BeginArea (new Rect (CharacterPositionBottomRight.x-(CharacterPositionBottomRight.x-CharacterPositionTopLeft.x)/2-(int)widthScreen*75/1000, Screen.height-CharacterPositionTopLeft.y-(int)heightScreen*4/100, (int)widthScreen*15/100, (int)heightScreen*4/100));
+			{
+				GUILayout.BeginVertical();
+				{
+					GUILayout.FlexibleSpace();
+					GUILayout.BeginHorizontal();
+					{
+						GUILayout.FlexibleSpace();
+						GUILayout.Label (Name+ "\u00A0" ,NamePoliceStyle);
+						GUILayout.Label (Life+ "\u00A0",LifePoliceStyle);
+						GUILayout.FlexibleSpace();
+					}
+					GUILayout.EndHorizontal();
+					GUILayout.FlexibleSpace();
+					GUILayout.BeginHorizontal();
+					{
+						GUILayout.FlexibleSpace();
+						GUILayout.Box(attackIcon,iconStyle);
+						GUILayout.Label ("\u00A0"+Attack+ "\u00A0",AttackPoliceStyle);
+						GUILayout.Box(quicknessIcon,iconStyle);
+						GUILayout.Label ("\u00A0"+Quickness+ "\u00A0",QuicknessPoliceStyle);
+						GUILayout.Box(moveIcon,iconStyle);
+						GUILayout.Label ("\u00A0"+Move+ "\u00A0",MovePoliceStyle);
+						GUILayout.FlexibleSpace();
+
+					}
+					GUILayout.EndHorizontal();
+					GUILayout.FlexibleSpace();
+				}
+				GUILayout.EndVertical();
+			}
+			GUILayout.EndArea ();
+		}
+
+	}
+
+	private void setStyles() {
+		heightScreen = Screen.height;
+		widthScreen = Screen.width;
+		this.NamePoliceStyle.fontSize = heightScreen*15/1000;
+		this.NamePoliceStyle.fixedHeight = (int)heightScreen*2/100;
+		this.SpeedPoliceStyle.fontSize = heightScreen*15/1000;
+		this.SpeedPoliceStyle.fixedHeight = (int)heightScreen*2/100;
+		this.AttackPoliceStyle.fontSize = heightScreen*15/1000;
+		this.AttackPoliceStyle.fixedHeight = (int)heightScreen*2/100;
+		this.MovePoliceStyle.fontSize = heightScreen*15/1000;
+		this.MovePoliceStyle.fixedHeight = (int)heightScreen*2/100;
+		this.QuicknessPoliceStyle.fontSize = heightScreen*15/1000;
+		this.QuicknessPoliceStyle.fixedHeight = (int)heightScreen*2/100;
+		this.LifePoliceStyle.fontSize = heightScreen*15/1000;
+		this.LifePoliceStyle.fixedHeight = (int)heightScreen*2/100;
+		this.iconStyle.fixedHeight=(int)heightScreen*2/100;
+	}
+
+	public void hideInformations(){
+		toHide=true;
+	}
+
+	public void showInformations(){
+		toHide=false;
+	}
+
+	public void setName(string name){
+		this.Name = name;
+	}
+
+	public void setLife(int life){
+		this.Life = life;
+	}
+
+	public void setAttack(int attack){
+		this.Attack = attack;
+	}
+
+	public void setMove(int move){
+		this.Move = move;
+	}
+
+	public void setQuickness(int quickness){
+		this.Quickness = quickness;
+	}
+
+	public void toWalk(){
+		animator.SetBool("isWalking",true );
+	}
+
+	public void stopWalking(){
+		animator.SetBool("isWalking",false );
+	}
+}
---
+++ Assets/Scripts/CharacterMakerScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMakerScript : MonoBehaviour {
+
+	GameObject character;
+	CharacterScript characterModelScript;
+
+	// Use this for initialization
+	void Start () {
+
+		characterModelScript = GameObject.Find("Character").GetComponent<CharacterScript> ();
+
+		characterModelScript.setName("Garruk");
+		characterModelScript.setLife(100);
+		characterModelScript.setMove(8);
+		characterModelScript.setQuickness(89);
+		characterModelScript.setAttack(78);
+		characterModelScript.showInformations();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Return)){
+			characterModelScript.toWalk();
+		}
+		if(Input.GetKeyUp(KeyCode.Return)){
+			characterModelScript.stopWalking();
+		}
+	
+	}
+}
