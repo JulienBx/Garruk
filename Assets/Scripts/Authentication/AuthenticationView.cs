346a4ba13a457b3876ce4647c08f6bcb2b3e8bf8
ggthemj <guillaume@oojump.com>
2015-04-08 16:57:40 +0200

    Commit boulet

--- Assets/Scripts/Authentication/AuthenticationView.cs
+++ Assets/Scripts/Authentication/AuthenticationView.cs
@@ -59,47 +59,4 @@ public class AuthenticationView : MonoBehaviour {
 //		}
 //		GUILayout.EndArea();
 	}
-
-	IEnumerator Login(string formNick, string formPassword) 
-	{
-//		if (formNick == "" || formPassword == "")
-//			yield break;
-//
-//		string memorize = "0";
-//		if (memorizeLogins)
-//			memorize = "1";
-//
-//		WWWForm form = new WWWForm(); 								// Création de la connexion
-//		form.AddField("myform_hash", ApplicationModel.hash); 		// hashcode de sécurité, doit etre identique à celui sur le serveur
-//		form.AddField("myform_nick", formNick);
-//		form.AddField("myform_pass", formPassword);
-//		form.AddField ("myform_memorize", memorize);
-//		form.AddField ("myform_macadress", macAdress);
-//		
-//		WWW w = new WWW(URLCheckAuthentification, form); 								// On envoie le formulaire à l'url sur le serveur 
-//		yield return w; 											// On attend la réponse du serveur, le jeu est donc en attente
-//		
-//		if (w.error != null) 
-//		{
-//			//print(w.error); 										// donne l'erreur eventuelle
-//		} 
-//		else 
-//		{
-//			//print(w.text);
-//			if (w.text.Equals("PASSWORD CORRECT")) 					// On affiche la page d'accueil si l'authentification réussie
-//			{ 				
-//				ApplicationModel.username = formNick;
-//				Application.LoadLevel("HomePage");
-//			}
-//			else 
-//			{
-//				resize = Screen.height/10;
-//				error = "informations incorrectes, " +			// on affiche les données transmises du serveur
-//					"veuillez réessayer: " + w.text;
-//			}
-//			//w.Dispose(); 											// on supprime la connexion
-//		}
-//		formPassword = ""; 											// On efface les variables
-		yield break;
-	}
 }
