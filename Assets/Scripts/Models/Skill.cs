89525a888e9160e7aa347ee7521bae438855e3b0
Yoann Bourges <yoann@oojump.com>
2015-05-28 15:44:49 +0200

    commit skill

--- Assets/Scripts/Models/Skill.cs
+++ Assets/Scripts/Models/Skill.cs
@@ -15,13 +15,15 @@ public class Skill
 	public int XMin;
 	public string Action;
 	public int nbLeft = 99 ;
+	public int CardType;
+	public Texture2D texture;
+	public string Picture;
 
 
 	public Skill(string name, int id, int isactivated, int level, int power, int manaCost, string description, string action) : this(name, id, isactivated, level, power, manaCost, description)
 	{
 		this.Action = action;
 	}
-
 	public Skill(string name, int id, int isactivated, int level, int power, int manaCost, string description)
 	{
 		this.Name = name;
@@ -36,7 +38,6 @@ public class Skill
 			nbLeft = 2 ;
 		}
 	}
-	
 	public Skill(string name, int id, int isactivated, int level, int power, int manaCost)
 	{
 		this.Name = name;
@@ -50,7 +51,6 @@ public class Skill
 			nbLeft = 2 ;
 		}
 	}
-
 	public Skill(string name)
 	{
 		this.Name = name;
@@ -60,10 +60,15 @@ public class Skill
 	{
 		this.Id = id;
 	}
-
 	public Skill()
 	{
 	
+	}
+	public IEnumerator setPicture()
+	{
+		var www = new WWW(ApplicationModel.host+this.Picture);
+		yield return www;
+		www.LoadImageIntoTexture(this.texture);
 	}
 
 }
