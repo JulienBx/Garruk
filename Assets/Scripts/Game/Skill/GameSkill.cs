ceaed2468f12b7b83bbd48664485822ecc6d78e9
ggthemj <guillaume@oojump.com>
2015-05-29 12:56:00 +0200

    Commit boulet2

--- Assets/Scripts/Game/Skill/GameSkill.cs
+++ Assets/Scripts/Game/Skill/GameSkill.cs
@@ -3,43 +3,47 @@ using System.Collections.Generic;
 
 public class GameSkill
 {
-	public virtual void launch()
-	{
-		Debug.Log("Skill non implémenté");
-	}
-
-	public virtual void setTarget(PlayingCardController pcc)
-	{
-		Debug.Log("Skill non implémenté");
-	}
-
-	public virtual void resolve(int[] args)
-	{
-		Debug.Log("Skill non implémenté");
-	}
-
-	public string convertStatToString(ModifierStat stat)
-	{
-		switch (stat)
+		public virtual void launch ()
 		{
-			case ModifierStat.Stat_Attack:
-				return "en attaque";
-				break;
-			case ModifierStat.Stat_Life:
-				return "de vie";
-				break;
-			case ModifierStat.Stat_Move:
-				return "en mouvement";
-				break;
-			case ModifierStat.Stat_Speed:
-				return "en vitesse";
-				break;
-			case ModifierStat.Stat_Dommage:
-				return "de dommage";
-				break;
-			default:
-				return "";
-				break;
+				Debug.Log ("Skill non implémenté");
 		}
-	}
+
+		public virtual void setTarget (PlayingCardController pcc)
+		{
+				Debug.Log ("Skill non implémenté");
+		}
+
+		public virtual void resolve (int[] args)
+		{
+				Debug.Log ("Skill non implémenté");
+		}
+
+		public virtual bool isLaunchable (int[] args)
+		{
+				return false;
+		}
+
+		public string convertStatToString (ModifierStat stat)
+		{
+				switch (stat) {
+				case ModifierStat.Stat_Attack:
+						return "en attaque";
+						break;
+				case ModifierStat.Stat_Life:
+						return "de vie";
+						break;
+				case ModifierStat.Stat_Move:
+						return "en mouvement";
+						break;
+				case ModifierStat.Stat_Speed:
+						return "en vitesse";
+						break;
+				case ModifierStat.Stat_Dommage:
+						return "de dommage";
+						break;
+				default:
+						return "";
+						break;
+				}
+		}
 }
