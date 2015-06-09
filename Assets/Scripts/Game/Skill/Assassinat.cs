c27831d672f2c23d3aee004e41210d448f171b26
ggthemj <guillaume@oojump.com>
2015-06-09 14:31:23 +0200

    Commit Assassinat

--- Assets/Scripts/Game/Skill/Assassinat.cs
+++ Assets/Scripts/Game/Skill/Assassinat.cs
@@ -1,11 +1,65 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class Assassinat : GameSkill
 {
-	/*	public override void launch()
+	public Assassinat()
 	{
-		Debug.Log ("Je lance assassinat");
-		//StatModifiers.Add(new StatModifier(Skill.Power, Skill.XMin, Skill.Ponderation, ModifierType.Type_BonusMalus, ModifierStat.Stat_Speed));
+		
 	}
-	*/
+	
+	public override void launch()
+	{
+		Debug.Log("Je lance assassinat");
+		GameController.instance.lookForAdjacentTarget("Choisir une cible à attaquer", "Lancer assassinat");
+	}
+	
+	public override void resolve(int[] args)
+	{
+		int targetID = args [0];
+		
+		int killpercentage = GameController.instance.getCurrentSkill().ManaCost;
+		int attack = GameController.instance.getCard(targetID).GetLife(); ;
+		
+		string message = GameController.instance.getCurrentCard().Title+" tente d'assassiner "+GameController.instance.getCard(targetID).Title;
+		
+		if (Random.Range(1, 100) > GameController.instance.getCard(targetID).GetEsquive()){
+			if (Random.Range(1, 100) <= killpercentage){
+				message += "\n"+"Le héros est assassiné";
+				GameController.instance.addModifier(targetID, attack, (int)ModifierType.Type_BonusMalus, (int)ModifierStat.Stat_Dommage);
+			}
+			else{
+				message += "\n"+"L'assassinat échoue";
+			}
+		}
+		else{
+			message += "\n"+GameController.instance.getCard(targetID).Title+" esquive l'attaque";
+		}
+		
+		GameController.instance.play(message);	
+	}
+	
+	public override bool isLaunchable(Skill s){
+		List<Tile> tempTiles;
+		Tile t = GameController.instance.getCurrentPCC().tile;
+		
+		tempTiles = t.getImmediateNeighbouringTiles();
+		bool isLaunchable = false ;
+		int i = 0 ;
+		int tempInt ; 
+		
+		while (!isLaunchable && i<tempTiles.Count){
+			t = tempTiles[i];
+			tempInt = GameController.instance.getTile(t.x, t.y).characterID;
+			if (tempInt!=-1)
+			{
+				if (GameController.instance.getPCC(tempInt).cannotBeTargeted==-1)
+				{
+					isLaunchable = true ;
+				}
+			}
+			i++;
+		}
+		return isLaunchable ;
+	}
 }
