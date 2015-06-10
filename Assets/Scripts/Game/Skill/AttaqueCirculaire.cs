ed8612f39097d7a1fac0805ca5012e5be931c2a1
ggthemj <guillaume@oojump.com>
2015-06-10 10:27:43 +0200

    Attaque circulaire

--- Assets/Scripts/Game/Skill/AttaqueCirculaire.cs
+++ Assets/Scripts/Game/Skill/AttaqueCirculaire.cs
@@ -10,28 +10,46 @@ public class AttaqueCirculaire : GameSkill
 	
 	public override void launch()
 	{
-		Debug.Log("Je lance attaque frontale");
-		GameController.instance.lookForAdjacentTarget("Choisir une cible à attaquer", "Lancer att. fro.");
+		Debug.Log("Je lance attaque circulaire");
+		GameController.instance.lookForAdjacentTarget("Choisir une cible à attaquer", "Lancer att. cir.");
 	}
 	
 	public override void resolve(int[] args)
 	{
-		int targetID = args [0];
+		List<Tile> tempTiles;
+		Tile t = GameController.instance.getCurrentPCC().tile;
 		
-		int degats = GameController.instance.getCurrentSkill().ManaCost / 2;
-		int attack = GameController.instance.getCurrentSkill().ManaCost ;
+		tempTiles = t.getImmediateNeighbouringTiles();
+		List<int> targets = new List<int>();
 		
-		string message = GameController.instance.getCurrentCard().Title+" porte une attaque frontale sur "+GameController.instance.getCard(targetID).Title;
+		int i = 0 ;
+		int tempInt ; 
+		string message = "" ;
 		
-		if (Random.Range(1, 100) > GameController.instance.getCard(targetID).GetEsquive()){
-			message += "\n"+GameController.instance.getCurrentCard().Title+" inflige "+attack+" dégats à "+GameController.instance.getCard(targetID).Title;
-			message += "\n"+GameController.instance.getCurrentCard().Title+" s'inflige "+attack+" dégats ";
-			
-			GameController.instance.addModifier(targetID, attack, (int)ModifierType.Type_BonusMalus, (int)ModifierStat.Stat_Dommage);
-			GameController.instance.addModifier(targetID, degats, (int)ModifierType.Type_BonusMalus, (int)ModifierStat.Stat_Dommage);
+		while (i<tempTiles.Count){
+			t = tempTiles[i];
+			tempInt = GameController.instance.getTile(t.x, t.y).characterID;
+			if (tempInt!=-1)
+			{
+				if (GameController.instance.getPCC(tempInt).cannotBeTargeted==-1)
+				{
+					targets.Add(tempInt);
+				}
+			}
+			i++;
 		}
-		else{
-			message += "\n"+GameController.instance.getCard(targetID).Title+" esquive l'attaque";
+		
+		int degats = GameController.instance.getCurrentSkill().ManaCost*GameController.instance.getCurrentCard().Attack/100;
+		message += "Attaque circulaire";
+		
+		for (int j = 0 ; j < targets.Count ; j++){
+			if (Random.Range(1, 100) > GameController.instance.getCard(targets[j]).GetEsquive()){
+				message += "\n"+GameController.instance.getCurrentCard().Title+" inflige "+degats+" dégats à "+GameController.instance.getCard(targets[j]).Title;
+				GameController.instance.addModifier(targets[j], degats, (int)ModifierType.Type_BonusMalus, (int)ModifierStat.Stat_Dommage);
+			}
+			else{
+				message += "\n"+GameController.instance.getCard(targets[j]).Title+" esquive l'attaque";
+			}
 		}
 		
 		GameController.instance.play(message);	
