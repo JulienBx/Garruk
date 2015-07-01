9aaeac0377e1b3286a88649d2137690cff1d488e
Yoann Bourges <yoann@oojump.com>
2015-07-01 21:52:15 +0200

    demo sprite

---
+++ Assets/testYBO.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class testYBO : MonoBehaviour
+{
+	public static testYBO instance;
+	public GameObject spriteNude;
+	public Texture2D[] spritesTextures;
+
+	private GameObject[] sprites;
+
+	private Texture2D atlas;
+	private Rect[] atlasRects;
+	private int textureSize;
+
+	void Start()
+	{
+		instance = this;
+		this.textureSize = 8192;
+		this.sprites=new GameObject[10];
+		this.atlas = new Texture2D(this.textureSize, this.textureSize);
+		this.atlasRects = atlas.PackTextures (spritesTextures, 2, this.textureSize);
+
+		for(int i=0;i<this.sprites.Length;i++)
+		{
+			this.sprites[i] = Instantiate(this.spriteNude) as GameObject;
+			this.sprites [i].GetComponent<SpriteRenderer> ().sprite = Sprite.Create(this.atlas,new Rect(this.atlasRects[i].x*atlas.width,this.atlasRects[i].y*atlas.height,this.atlasRects[i].width*atlas.width,this.atlasRects[i].height*atlas.height),new Vector2(0.5f,0.5f));;
+			this.sprites[i].transform.position=new Vector3(-5+i,0,0);
+		}
+	}
+}
