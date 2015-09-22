a74a3245562032305dd08a9a1b29bdaa52e23a42
ggthemj <guillaume@oojump.com>
2015-09-22 11:40:14 +0200

    Commit boulet

--- Assets/Scripts/Game/SkillPopUpController.cs
+++ Assets/Scripts/Game/SkillPopUpController.cs
@@ -3,29 +3,21 @@ using System.Collections.Generic;
 using System;
 using TMPro;
 
-public class AttackPictoController : MonoBehaviour
+public class SkillPopUpController : MonoBehaviour
 {	
 	bool isHovered = false ;
-	int idCard = 2 ;
+	int timeDoDisplay = 2 ;
 	
-	public void OnMouseEnter(){
-		if (!isHovered){
-			Vector3 position = gameObject.transform.position;
-			position.y += 0.65f;
-			GameView.instance.displayPopUp("Le héros inflige "+GameView.instance.getCard(idCard).GetAttack()+" dégats de base à chaque attaque", position, "Attaque");
-			this.isHovered = true ;
-		}
+	public void Awake(){
+		//gameObject.transform.FindChild("Receiver0").GetComponent
 	}
 	
-	public void OnMouseExit(){
-		if (isHovered){
-			GameView.instance.hidePopUp();
-			this.isHovered = false ;
-		}
+	public void setTimeToDisplay(int a){
+		this.timeDoDisplay = a 	;
 	}
 	
-	public void setIDCard(int i){
-		this.idCard = i ;
+	public void getTimeToDisplay(int a){
+		this.timeDoDisplay = a 	;
 	}
 }
 
