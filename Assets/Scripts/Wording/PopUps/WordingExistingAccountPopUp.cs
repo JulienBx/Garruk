5f13e18e64458ea5fa63d160d2d98a1e60851547
YoannBourges <yoann@imac-de-julia-alegre.home>
2016-03-11 19:35:19 +0100

    commit intermédiaire

--- Assets/Scripts/Wording/PopUps/WordingExistingAccountPopUp.cs
+++ Assets/Scripts/Wording/PopUps/WordingExistingAccountPopUp.cs
@@ -14,7 +14,7 @@ public class WordingExistingAccountPopUp
 	static WordingExistingAccountPopUp()
 	{
 		references=new List<string[]>();
-		references.Add(new string[]{"Merci de remplir les informations suivantes pour finaliser la création de votre compte","Please fill the following form to complete your registration"}); //0
+		references.Add(new string[]{"Saisissez vos identifiants habituels","Please fill your credentials"}); //0
 		references.Add(new string[]{"Pseudo","Player name"}); //1
 		references.Add(new string[]{"Mot de passe","Password"}); //3
 		references.Add(new string[]{"Valider","Confirm"}); //4
