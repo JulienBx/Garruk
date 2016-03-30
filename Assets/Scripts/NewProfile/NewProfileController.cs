d2316db5a89c07b2e78b3c80834973d59191eaab
YoannBourges <yoann@oojump.com>
2016-03-30 22:05:44 +0200

    commit modif

--- Assets/Scripts/NewProfile/NewProfileController.cs
+++ Assets/Scripts/NewProfile/NewProfileController.cs
@@ -1315,13 +1315,11 @@ public class NewProfileController : MonoBehaviour
 				{
 					gameTypeName=WordingGameModes.getReference(8);
 					idCompetitionPicture=1;
-					break;
 				}
 				else
 				{
 					gameTypeName=WordingGameModes.getReference(9);
 					idCompetitionPicture=0;
-					break;
 				}
 				this.resultsContents[i].transform.FindChild("title").GetComponent<TextMeshPro>().text=gameTypeName;
 				this.resultsContents[i].transform.FindChild("picture").GetComponent<SpriteRenderer>().sprite=BackOfficeController.instance.returnCompetitionPicture(idCompetitionPicture);
