0395958c396f9ba37765500e9eb2fee28df988dd
YoannBourges <yoann@imac-de-julia-alegre.home>
2016-04-21 10:25:51 +0200

    commit boulet

--- Assets/Scripts/Authentication/AuthenticationController.cs
+++ Assets/Scripts/Authentication/AuthenticationController.cs
@@ -942,7 +942,7 @@ public class AuthenticationController : Photon.MonoBehaviour
 	}
 	void OnJoinedLobby()
 	{
-		if(ApplicationModel.player.ToDeconnect=false)
+		if(!ApplicationModel.player.ToDeconnect)
 		{
 			this.loadLevels();
 		}
