6260d2030480a19675fa8a82c232e5f40a5bce2a
ggthemj <guillaume@oojump.com>
2016-04-25 18:57:02 +0200

    CGC

--- Assets/Scripts/Game/Skill/Furie.cs
+++ Assets/Scripts/Game/Skill/Furie.cs
@@ -26,7 +26,7 @@ public class Furie : GameSkill
 	public override void applyOnMe(int target){
 		GameCard currentCard = GameView.instance.getCurrentCard();
 		int level = GameView.instance.getCurrentSkill().Power;
-		int bonusLife =  Mathf.Min(currentCard.GetTotalLife()-currentCard.getLife(),level*2+10);
+		int bonusLife = level*2+10;
 		int bonusAttack = 5+level;
 		target = GameView.instance.getCurrentPlayingCard();
 
@@ -34,14 +34,63 @@ public class Furie : GameSkill
 		if(bonusLife>0){
 			text+="\n+"+bonusLife+"PV";
 		}
-		 
-		GameView.instance.getPlayingCardController(target).addDamagesModifyer(new Modifyer(-1*bonusLife, -1, 93, base.name, "+"+bonusLife+"PV"), false);
+
 		GameView.instance.getPlayingCardController(target).updateAttack(currentCard.getAttack());
 		GameView.instance.getPlayingCardController(target).addAttackModifyer(new Modifyer(bonusAttack, -1, 93, base.name, ". Permanent"));
+		GameView.instance.getPlayingCardController(target).updateLife(currentCard.getLife());
+		GameView.instance.getPlayingCardController(target).addPVModifyer(new Modifyer(bonusLife, -1, 93, base.name, ". Permanent"));
+
 		GameView.instance.getCard(target).setFurious(new Modifyer(0, -1, 93, base.name, "Incontrolable!"));
 		GameView.instance.getPlayingCardController(target).showIcons();
 
 		GameView.instance.displaySkillEffect(target, text, 1);
 		GameView.instance.addAnim(GameView.instance.getTile(target), 93);
+	}
+
+	public override int getActionScore(Tile t, Skill s){
+		GameCard currentCard = GameView.instance.getCurrentCard();
+		GameCard targetCard ;
+		int score = 50 - (currentCard.getLife()+10+2*s.Power)+10+2*s.Power;
+
+		List<int> everyone = GameView.instance.getEveryoneButMe();
+		int smallestMove = 10; 
+		bool isSmallestMoveEnnemi = false ;
+		int nbEnnemis =0; 
+		int nbAllies =0;
+		int moveAllies =0;
+		int moveEnnemis =0;
+		int move ;
+		Tile tile ;
+		for(int i = 0 ; i < everyone.Count ; i++){
+			targetCard = GameView.instance.getCard(everyone[i]);
+			tile = GameView.instance.getTile(everyone[i]);
+			move = Mathf.Abs(t.x-tile.x)+Mathf.Abs(t.y-tile.y);
+			if(move<=smallestMove){
+				if(move==smallestMove){
+					if(!targetCard.isMine){
+						isSmallestMoveEnnemi = false ;
+					}
+				}
+				else{
+					smallestMove = move ;
+					isSmallestMoveEnnemi = targetCard.isMine;
+				}
+			}
+			if(targetCard.isMine){
+				nbEnnemis++;
+				moveEnnemis+=move;
+			}
+			else{
+				nbAllies++;
+				moveAllies+=move;
+			}
+		}
+
+		if(!isSmallestMoveEnnemi){
+			score-=100;
+		}
+		score += Mathf.RoundToInt(100f*((1.0f*moveAllies/nbAllies)-(1.0f*moveEnnemis/nbEnnemis)));
+				
+		return score ;
 	}
 }
