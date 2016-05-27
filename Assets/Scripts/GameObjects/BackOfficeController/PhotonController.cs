31364404960348411c9e98729bb4aecf6c0ca20e
YoannBourges <yoann@imac-de-julia-alegre.home>
2016-05-27 11:49:09 +0200

    commit

--- Assets/Scripts/GameObjects/BackOfficeController/PhotonController.cs
+++ Assets/Scripts/GameObjects/BackOfficeController/PhotonController.cs
@@ -91,7 +91,7 @@ public class PhotonController : Photon.MonoBehaviour
     {
         print("La room n'existe plus");
         BackOfficeController.instance.loadScene("NewHomePage");
-        BackOfficeController.instance.displayErrorPopUp(WordingServerError.getReference(22));
+        BackOfficeController.instance.displayErrorPopUp(WordingServerError.getReference("22",true));
     }
 
     public void CreateNewRoom()
